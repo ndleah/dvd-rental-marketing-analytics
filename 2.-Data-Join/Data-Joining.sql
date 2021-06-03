75e39a957eadf9f9860c70c895027b894cacc81e
Leah Nguyen <nduongthucanh@gmail.com>
2021-06-03 18:19:45 +0700

    update queries

--- 2.-Data-Join/Data-Joining.sql
+++ 2.-Data-Join/Data-Joining.sql
@@ -1,7 +1,8 @@
 /********************* 
 DATA JOINING: 
 **********************/
-
+--I. JOINING TABLES
+--create complete joint dataset
 DROP TABLE IF EXISTS complete_joint_dataset;
 CREATE TEMP TABLE complete_joint_dataset AS
 SELECT
@@ -36,3 +37,55 @@ SELECT * FROM complete_joint_dataset limit 10;
 |126        |565    |MATRIX SNOWMAN |9          |Foreign      |
 |399        |396    |HANGING DEEP   |7          |Drama        |
 */
+
+
+--II. DEALING WITH TIES 
+--create complete joint dataset with additional "rental_date" column
+DROP TABLE IF EXISTS complete_joint_dataset_with_rental_date;
+CREATE TEMP TABLE complete_joint_dataset_with_rental_date AS
+SELECT
+  rental.customer_id,
+  inventory.film_id,
+  film.title,
+  category.name AS category_name,
+  rental.rental_date
+FROM dvd_rentals.rental
+INNER JOIN dvd_rentals.inventory
+  ON rental.inventory_id = inventory.inventory_id
+INNER JOIN dvd_rentals.film
+  ON inventory.film_id = film.film_id
+INNER JOIN dvd_rentals.film_category
+  ON film.film_id = film_category.film_id
+INNER JOIN dvd_rentals.category
+  ON film_category.category_id = category.category_id
+
+--perform group by aggregations on category_name and customer_id
+SELECT
+  customer_id,
+  category_name,
+  COUNT(*) AS rental_count,
+  MAX(rental_date) AS latest_rental_date
+FROM complete_joint_dataset_with_rental_date
+GROUP BY 
+  customer_id,
+  category_name
+ORDER BY
+  customer_id,
+  rental_count DESC,
+  latest_rental_date DESC
+LIMIT 10;
+
+ /*Result: 
+|customer_id|category_name|rental_count|latest_rental_date      |
+|-----------|-------------|------------|------------------------|
+|1          |Classics     |6           |2005-08-19T09:55:16.000Z|
+|1          |Comedy       |5           |2005-08-22T19:41:37.000Z|
+|1          |Drama        |4           |2005-08-18T03:57:29.000Z|
+|1          |Animation    |2           |2005-08-22T20:03:46.000Z|
+|1          |Sci-Fi       |2           |2005-08-21T23:33:57.000Z|
+|1          |New          |2           |2005-08-19T13:56:54.000Z|
+|1          |Action       |2           |2005-08-17T12:37:54.000Z|
+|1          |Music        |2           |2005-07-09T16:38:01.000Z|
+|1          |Sports       |2           |2005-07-08T07:33:56.000Z|
+|1          |Family       |1           |2005-08-02T18:01:38.000Z|
+ */
